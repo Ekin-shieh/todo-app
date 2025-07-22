cef9890e79534eb0bc9c705a2e9274deed26dd95
Ekin <1197026828@qq.com>
2025-07-22 16:28:09 +1000

    change the structure of css

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const model = document.getElementById('change-model');
 const container = document.getElementsByClassName('container')[0];
-const list = document.getElementById('todo-list');
 
 model.addEventListener('click', ()=>{
     if (model.src.includes('./images/incon-sun.svg')) {
@@ -10,16 +9,3 @@ model.addEventListener('click', ()=>{
     };
     container.classList.toggle('night');
 });
-
-list.addEventListener('click', (e)=>{
-    const target = e.target;
-    const deleteBtn = target.closest(".delete");
-    const completeBtn = target.closest(".circle");
-    if(completeBtn) {
-        const item =target.closest('.todo-item');
-        const text = item.querySelector('.text');
-        const img =item.querySelector('.circle-img');
-        text.classList.toggle('completed');
-        img.classList.toggle('completed');
-    }
-})
